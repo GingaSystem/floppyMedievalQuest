77dba475bdd62a7c5698da4c396cac6abcef579b
GingaSystem <g17939oa@gm.tsuda.ac.jp>
2021-10-11 01:40:13 +0900

    Initial commit

---
+++ Assets/cube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("cubeだよ！");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.UpArrow)){
+            transform.position += transform.forward;
+        }else if(Input.GetKey(KeyCode.DownArrow)){
+            transform.position -= transform.forward;
+        }
+	}
+}
